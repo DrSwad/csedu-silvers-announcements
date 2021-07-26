d6b035ecb400824782787c7c7701b615e5ff2a2d
Nayeemul Islam Swad <drswad2013@gmail.com>
2021-07-26 19:25:45 +0600

    :rewind: revert: remove facebook webhook verification

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,7 +3,6 @@
 import announceInClassroom from "../../helpers/classroom"
 
 export default async function handler(req, res) {
-  console.log(req)
   // await announceInClassroom('New classroom announcement')
-  res.status(200).send(req.query['hub.challenge'])
+  res.status(200).send('Request received')
 }
