3ba41e2646da5f627a45b01825b7644b35a7d39d
Jaian25 <jaiancsedu25@gmail.com>
2021-07-27 21:16:13 +0600

    Integrated discord webhook

--- helpers/discord.js
+++ helpers/discord.js
@@ -1,6 +1,15 @@
 export default async function announceInDiscord(message) {
   try {
-    // code for sending `message` to some chat
-    // chat ID (or whatever is required to send messages via discord API) can be hardcoded for now
+    const Discord = require('discord.js')
+    const webhook = new Discord.WebhookClient('869593915722989588','X9I5x3yM_1P9-K_QRMIt7D9vPSjhigCrWwdKsGv5bAp5gKGyUKveEGHHkaYQi1SJWFj1')
+    const messageEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    // .setTitle('CSEDU_Silvers')
+    // .setURL('https://www.facebook.com/groups/2524530824240025')
+    .setAuthor('CSEDU_Silvers', 'https://i.ibb.co/yk1CLxT/download.png','https://www.facebook.com/groups/2524530824240025')
+    .addField('**Notice**', message, true)
+    .setTimestamp()
+    .setFooter('Message sent timing: ', 'https://i.ibb.co/yk1CLxT/download.png');
+    webhook.send(messageEmbed)
   } catch(err) { console.error(err) }
 }
