72085c740a121ef5393f773a19a5295a8d7f2001
Nayeemul Islam Swad <drswad2013@gmail.com>
2021-07-27 13:29:33 +0600

    :construction: feat: discord announcement boilerplate

---
+++ helpers/discord.js
@@ -0,0 +1,6 @@
+export default async function announceInDiscord(message) {
+  try {
+    // code for sending `message` to some chat
+    // chat ID (or whatever is required to send messages via discord API) can be hardcoded for now
+  } catch(err) { console.error(err) }
+}
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import announceInDiscord from "../../helpers/discord"
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  announceInDiscord('New discord announcement')
+  res.status(200).send('Request received')
 }
