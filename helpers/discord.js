788625938869c4351f0e5f61fa1eaea70a504d33
Nayeemul Islam Swad <drswad2013@gmail.com>
2021-07-28 11:13:42 +0600

    :recycle: reformat: import package outside function and use environment variables

--- helpers/discord.js
+++ helpers/discord.js
@@ -1,15 +1,23 @@
+const Discord = require("discord.js");
+const webhook = new Discord.WebhookClient(
+  process.env["DISCORD_CLIENT_ID"],
+  process.env["DISCORD_CLIENT_TOKEN"]
+);
+
 export default async function announceInDiscord(message) {
   try {
-    const Discord = require('discord.js')
-    const webhook = new Discord.WebhookClient('869593915722989588','X9I5x3yM_1P9-K_QRMIt7D9vPSjhigCrWwdKsGv5bAp5gKGyUKveEGHHkaYQi1SJWFj1')
     const messageEmbed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    // .setTitle('CSEDU_Silvers')
-    // .setURL('https://www.facebook.com/groups/2524530824240025')
-    .setAuthor('CSEDU_Silvers', 'https://i.ibb.co/yk1CLxT/download.png','https://www.facebook.com/groups/2524530824240025')
-    .addField('**Notice**', message, true)
-    .setTimestamp()
-    .setFooter('Message sent timing: ', 'https://i.ibb.co/yk1CLxT/download.png');
-    webhook.send(messageEmbed)
-  } catch(err) { console.error(err) }
+      .setColor("#0099ff")
+      .setAuthor(
+        "CSEDU_Silvers",
+        process.env["CSEDU_ICON_URL"],
+        process.env["FACEBOOK_GROUP_URL"]
+      )
+      .addField("**Notice**", message, true)
+      .setTimestamp()
+      .setFooter("Message sent timing: ", process.env["CSEDU_ICON_URL"]);
+    webhook.send(messageEmbed);
+  } catch (err) {
+    console.error(err);
+  }
 }
